90cbf12afa79afb0e1cdf10f191b5faacef3fd97
Rudraksha-code <rm1ov07@limestone.on.ca>
2025-04-21 16:00:14 -0400

    Student.java now contains OOP code to initalize each student separately
    Used super which to call the constructor of the parent class.

--- Student.java
+++ Student.java
@@ -1,3 +1,31 @@
-class Student extends Person{
-    
+import java.util.ArrayList;
+
+class Student extends Person {
+    private ArrayList<Integer> marks;
+    private ArrayList<Integer> attendance;
+
+    public Student(String id, String name, String dob, ArrayList<Integer> marks, ArrayList<Integer> attendance) {
+        // used super to call the constructor of the parent class (Person)
+        super(id, name, dob);
+
+        // Initialize Student-specific fields
+        this.marks = marks;
+        this.attendance = attendance;
+    }
+
+    public ArrayList<Integer> getMarks() {
+        return marks;
+    }
+
+    public ArrayList<Integer> getAttendance() {
+        return attendance;
+    }
+
+    public void setMarks(ArrayList<Integer> marks) {
+        this.marks = marks;
+    }
+
+    public void setAttendance(ArrayList<Integer> attendance) {
+        this.attendance = attendance;
+    }
 }
