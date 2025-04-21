28cc68cd00da15cc204020f5bfa2a363faf115d9
Rudraksha-code <rm1ov07@limestone.on.ca>
2025-04-21 16:17:38 -0400

    Similar as studetn.java for teacher.java as they both inherit data from person.java

--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 class Teacher extends Person {
-    
+    private ArrayList<String> courses;
+
+    public Teacher(String id, String name, String dob, ArrayList<String> courses) {
+        super(id, name, dob);
+        this.courses = courses;
+    }
+
+    public ArrayList<String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<String> courses) {
+        this.courses = courses;
+    }
 }
